9a0d72cfd28abb935209837933f754a48a0dde43
pgmorgan <petergm@gmail.com>
2019-08-29 12:57:41 -0400

    I am moving this back to Master Branch

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func main() {
 	http.HandleFunc("/insert", handler.Insert)
 	http.HandleFunc("/delete", handler.Delete)
 	http.HandleFunc("/", handler.Index)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":80", nil)
 	// handler.Index()
 }
